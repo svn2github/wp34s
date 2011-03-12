4e0b649ec527ee3a8823153671d8429848acdc06
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-03-12 23:02:03 +0000

    Added Windows DLL export declarations
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@124 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/xeq.h
+++ trunk/xeq.h
@@ -724,7 +724,11 @@ typedef struct _ram {
 
 } TPersistentRam;
 
+#ifdef WINGUI
+extern __declspec(dllimport) TPersistentRam PersistentRam;
+#else
 extern TPersistentRam PersistentRam;
+#endif
 
 #define State		(PersistentRam._state)
 #define DispMsg		(PersistentRam._disp_msg)
