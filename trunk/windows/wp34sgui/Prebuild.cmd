e0adc4f60aa9b430205e846aa0de5785471f121d
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-03-20 12:30:55 +0000

    Updated creation of revision.txt in project root by Windows build.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@263 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/wp34sgui/Prebuild.cmd
+++ trunk/windows/wp34sgui/Prebuild.cmd
@@ -1,4 +1,16 @@
-touch ..\..\..\revision.txt
+@echo off
+:
+:  Create the SDK file builddate.h
+:  Force an update of revision.txt in project root for SVN
+:
+setlocal
+set lang=en
+set TZ=UTC
+set dest=..\..\..\revision.txt
+copy revision.txt.template %dest%
+echo Last Windows compile >> %dest%
+_date.exe >> %dest%
+
 del builddate.h
 CreateDate.exe builddate.h xeq.h
 echo #define SVN_REVISION "$Rev::       $">>builddate.h
