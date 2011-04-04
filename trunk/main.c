85e0f04bcda8beb974c2d62a5d96814dd1f4a9cc
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-04-04 18:39:07 +0000

    Check in for backup purposes
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@496 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/main.c
+++ trunk/main.c
@@ -21,6 +21,7 @@
 #include "xeq.h"
 
 #include "atmel/board.h"
+#include "atmel/aic.h"
 #include "atmel/pit.h"
 #include "atmel/supc.h"
 #include "atmel/slcdc.h"
@@ -49,9 +50,6 @@
 #define PLLMUL            999
 /// PLL DIV value.
 #define PLLDIV            1
-// Helper
-#define wait_for_clock() while ( ( AT91C_BASE_PMC->PMC_SR & AT91C_PMC_MCKRDY ) \
-				 != AT91C_PMC_MCKRDY )
 
 /*
  *  Setup the perstent RAM
@@ -65,23 +63,32 @@ unsigned int ClockSpeed;
 unsigned int Heartbeats;
 unsigned int Contrast;
 
+/*
+ *  Definitions for the keyboard scan
+ */
+#define KEY_BUFF_SHIFT 4
+#define KEY_BUFF_LEN ( 1 << KEY_BUFF_SHIFT )
+#define KEY_BUFF_MASK ( KEY_BUFF_LEN - 1 )
+signed char KeyBuffer[ KEY_BUFF_LEN ];
+int KbRead, KbWrite, KbCount;
+union _ll {
+	unsigned char c[ 8 ];
+	unsigned long long ll;
+} KbData, KbDebounce;
+
+#define KEY_ROWS_MASK 0x0000007f
+#define KEY_COLS_MASK 0x0400f800
+#define KEY_COLS_SHIFT 11
+#define KEY_COL5_SHIFT 10
+#define KEY_ON_MASK   0x00000400
 
 /*
- *  Go to idle mode
- *  Runs from SRAM to be able to turn of flash
+ *  Short wait to allow output lines to settle
  */
-RAM_FUNCTION void go_idle( void )
+int short_wait( int count )
 {
-	/*
-	 *  Disable flash memory in order to save power
-	 */
-	SUPC_DisableFlash();
-
-	/*
-	 *  Disable the processor clock and go to sleep
-	 */
-	AT91C_BASE_PMC->PMC_SCDR = AT91C_PMC_PCK;   
-	while ( ( AT91C_BASE_PMC->PMC_SCSR & AT91C_PMC_PCK ) != AT91C_PMC_PCK ); 
+	while ( count-- );
+	return count;
 }
 
 
@@ -90,6 +97,84 @@ RAM_FUNCTION void go_idle( void )
  */
 void scan_keyboard( void )
 {
+	int k;
+
+	/*
+	 *  Program the PIO pins in PIOC
+	 */
+	// Enable clock
+	AT91C_BASE_PMC->PMC_PCER = 1 << AT91C_ID_PIOC;
+	// Rows as open drain output
+	AT91C_BASE_PIOC->PIO_OER = KEY_ROWS_MASK;
+	AT91C_BASE_PIOC->PIO_MDDR = KEY_ROWS_MASK;
+	
+	/*
+	 *  Quick check
+	 *  Set all rows to low and test if any key input is low
+	 */
+	AT91C_BASE_PIOC->PIO_CODR = KEY_ROWS_MASK;
+	short_wait( 1 );
+	k = AT91C_BASE_PIOC->PIO_PDSR;
+	AT91C_BASE_PIOC->PIO_SODR = KEY_ROWS_MASK;
+
+	if ( KEY_COLS_MASK == ( k & KEY_COLS_MASK ) ) {
+		/*
+		 *  No key is down
+		 */
+		KbData.ll = 0ll;
+	}
+	else {
+		/*
+		 *  Assemble the input
+		 */
+		int r, i, on;
+		for ( r = 1, i = 0; i < 8; r <<= 1, ++i ) {
+			/*
+			 *  Set each column to zero and read the row image
+			 */
+			AT91C_BASE_PIOC->PIO_CODR = r;
+			short_wait( 1 );
+			k = AT91C_BASE_PIOC->PIO_PDSR & KEY_COLS_MASK; 
+			AT91C_BASE_PIOC->PIO_SODR = r;
+			/*
+			 *  Adjust the result
+			 */
+			on = k & KEY_ON_MASK;
+			k >>= KEY_COLS_SHIFT;
+			k = ( k >> KEY_COL5_SHIFT ) | k;
+			KbData.c[ i ] &= (unsigned char) ( KbData.c[ i ] ^ k );
+		}
+		if ( on == 0 ) {
+	}
+
+
+	/*
+	 *  Reprogram PIO
+	 */
+	// All as input
+	AT91C_BASE_PIOC->PIO_ODR = KEY_ROWS_MASK | KEY_COLS_MASK;
+	// Disable clock
+	AT91C_BASE_PMC->PMC_PCDR = 1 << AT91C_ID_PIOC;
+
+
+
+
+	/*
+	 *  Mask with debounce image
+	 */
+
+	/*
+	 *  Store new debounce image
+	 */
+
+	/*
+	 *  Decode
+	 */
+
+	/*
+	 *  Add to buffer
+	 */
+
 }
 
 
@@ -166,6 +251,34 @@ void disable_lcd( void )
 
 
 /*
+ *  Go to idle mode
+ *  Runs from SRAM to be able to turn of flash
+ */
+RAM_FUNCTION void go_idle( void )
+{
+	/*
+	 *  Disable flash memory in order to save power
+	 */
+	SUPC_DisableFlash();
+
+	/*
+	 *  Disable the processor clock and go to sleep
+	 */
+	AT91C_BASE_PMC->PMC_SCDR = AT91C_PMC_PCK;   
+	while ( ( AT91C_BASE_PMC->PMC_SCSR & AT91C_PMC_PCK ) != AT91C_PMC_PCK ); 
+}
+
+
+/*
+ *  Helpers for set_speed
+ */
+#define wait_for_clock() while ( ( AT91C_BASE_PMC->PMC_SR & AT91C_PMC_MCKRDY ) \
+				 != AT91C_PMC_MCKRDY )
+#define disable_pll()  ( AT91C_BASE_PMC->PMC_PLLR = 0x8000 )
+#define disable_mclk() ( AT91C_BASE_PMC->PMC_MOR = 0x370000 )
+#define enable_mclk()  ( AT91C_BASE_PMC->PMC_MOR = 0x370001 )
+
+/*
  *  Set clock speed to one of 4 fixed values:
  *  0 (idle), 32.768 kHz, 2 MHz, 32.768 MHz
  */
@@ -198,22 +311,28 @@ void set_speed( unsigned int speed )
 		 */
 		AT91C_BASE_PMC->PMC_MCKR = AT91C_PMC_CSS_SLOW_CLK;
 		wait_for_clock();
+
 		// No wait states for flash read
 		AT91C_BASE_MC->MC_FMR = AT91C_MC_FWS_0FWS;
-		// Turn off unneccesary oscilators
-		// TODO
+		
+		// Turn off unneccesary oscillators (PLL and main)
+		disable_pll();
+		disable_mclk();
 		break;
 
 	case SPEED_MEDIUM:
 		/*
 		 *  2 MHz interal RC clock
 		 */
+		enable_mclk();
 		AT91C_BASE_PMC->PMC_MCKR = AT91C_PMC_CSS_MAIN_CLK;
 		wait_for_clock();
-		// No wait states for flash read
+		
+		// No wait states needed for flash read
 		AT91C_BASE_MC->MC_FMR = AT91C_MC_FWS_0FWS;
+		
 		// Turn off the PLL
-		// TODO
+		disable_pll();
 		break;
 
 	case SPEED_HIGH:
@@ -238,6 +357,9 @@ void set_speed( unsigned int speed )
 		// Switch to PLL
 		AT91C_BASE_PMC->PMC_MCKR = AT91C_PMC_CSS_PLL_CLK;
 		wait_for_clock();
+
+		// Turn off main clock
+		disable_mclk();
 		break;
 	}
 	ClockSpeed = speeds[ speed ];
@@ -268,7 +390,7 @@ void user_heartbeat( void )
 	if ( ++Keyticks > 1000 ) {
 		Keyticks = 1000;
 	}
-        // AddKey( K_HEARTBEAT, true );  // add only if buffer is empty
+        put_key( K_HEARTBEAT );  // add only if buffer is empty
 }
 
 
@@ -279,7 +401,7 @@ void user_heartbeat( void )
 RAM_FUNCTION void heartbeat_irq( void )
 {
 	int count;
-
+	
 	/*
 	 *  Flash memory might be disabled, turn it on again
 	 */
@@ -287,6 +409,17 @@ RAM_FUNCTION void heartbeat_irq( void )
 		SUPC_EnableFlash( 2 );  // ~ 130 microseconds wakeup at 32768 Hz
 	}
 
+	/*
+	 *  Since all system peripherals are tied to the same IRQ source 1
+	 *  we need to check, if this is really the PIT interrupt
+	 */
+	if ( !PIT_GetStatus() ) {
+		/*
+		 *  Add other sources here
+		 */
+		return;
+	}
+	
 	/*
 	 *  Get the number of missed interrupts
 	 */
@@ -326,16 +459,108 @@ RAM_FUNCTION void heartbeat_irq( void )
 }
 
 
+/*
+ *  Enable the PIT to generate interrupts
+ *  Do this after setting the speed so that the PIV value is already set.
+ */
+void enable_heartbeat()
+{
+	/*
+	 *  Tell the interrupt controller where to go
+	 */
+	AIC_ConfigureIT( AT91C_ID_SYS, 
+		         AT91C_AIC_SRCTYPE_INT_HIGH_LEVEL 
+		         | AT91C_AIC_PRIOR_HIGHEST,
+		         heartbeat_irq );
+	/*
+	 *  Enable IRQ 1
+	 */
+	AIC_EnableIT( AT91C_ID_SYS );
+
+	/*
+	 *  Enable the PIT and its interrupt
+	 */
+	PIT_EnableIT();
+	PIT_Enable();
+}
+
+
+/*
+ *  Lock / unlock by disabling / enabling the PIT interrupt.
+ *  Do nothing if called inside the interrupt handler
+ */
+void lock( void )
+{
+	if ( AT91C_BASE_AIC->AIC_ISR != AT91C_ID_SYS ) {
+		AIC_DisableIT( AT91C_ID_SYS );
+	}
+}
+
+void unlock( void )
+{
+	if ( AT91C_BASE_AIC->AIC_ISR != AT91C_ID_SYS ) {
+		AIC_EnableIT( AT91C_ID_SYS );
+	}
+}
+
+
 /*************************************************************************
  *  Entry points called from the application
  *************************************************************************/
 
 /*
  *  Check if something is waiting for attention
+ *  Returns the number of keys in the buffer
  */
-int is_key_pressed(void)
+int is_key_pressed( void )
 {
-        return 0;
+        return KbCount;
+}
+
+
+/*
+ *  Get a key
+ */
+int get_key( void )
+{
+	int k;
+	if ( KbCount == 0 ) {
+		/*
+		 *  No key in buffer
+		 */
+		return -1;
+	}
+	lock();
+	k =  KeyBuffer[ KbRead ];
+	KbRead = ( KbRead + 1 ) & KEY_BUFF_MASK;
+	--KbCount;
+	unlock();
+}
+
+
+/*
+ *  Add a key to the buffer
+ */
+int put_key( int k )
+{
+	if ( KbCount == KEY_BUFF_LEN ) {
+		/*
+		 *  Sorry, no room
+		 */
+		return;
+	}
+
+	if ( k == K_HEARTBEAT && KbCount != 0 ) {
+		/*
+		 *  Don't fill the buffer with heartbeats
+		 */
+		return;
+	}
+	lock();
+	KbWrite = ( KbWrite + 1 ) & KEY_BUFF_MASK;
+	KeyBuffer[ KbWrite ] = (unsigned char) k;
+	++KbCount;
+	unlock();
 }
 
 
@@ -348,11 +573,6 @@ void shutdown( void )
 	 *  Turn off display gracefully
 	 */
 	disable_lcd();
-
-	/*
-	 *  Backup sram power is still needed
-	 */
-	SUPC_EnableSram();
 
 	/*
 	 *  Off we go...
@@ -377,22 +597,39 @@ void watchdog( void )
 int main(void)
 {
         /*
-         * init hardware (LCD, timer, etc.)
+         * Initialize the hardware (clock, LCD, RTC, timer, backup)
 	 */
 	set_speed( SPEED_MEDIUM );
 	enable_lcd();
 	SUPC_EnableRtc();
-
-
+	enable_heartbeat();
+	SUPC_EnableSram();
 
 	/*
-                forever
-                        if key then
-                                full_speed
-                                while key process_keycode
-                        else idle
-        */
-        process_keycode(0);
+	 *  Initialize the software
+	 */
+	init_34s();
+
+	/*
+	 *  Wait for event and execute it
+	 */
+	while( 1 ) {
+                int k;
+		while ( !is_key_pressed() ) {
+			/*
+			 *  Save power
+			 */
+			go_idle();
+		}
+		k = get_key();
+		if ( k != K_HEARTBEAT ) {
+			/*
+			 *  Increase the speed of operation
+			 */
+			set_speed( SPEED_HIGH );
+		}
+		process_keycode( k );
+	}
         return 0;
 }
 
