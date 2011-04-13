09c755604b41811f56fab55946c06294fda36856
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-04-13 06:28:29 +0000

    Various fixes from last night
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@591 9654ca62-1852-461c-aa49-1be32f3bb02d

--- make.cmd
+++ make.cmd
@@ -1,5 +1,6 @@
 @setlocal
 @cd trunk\windows
+call "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 MSBuild wp34s_Express.sln /property:Configuration=Release > build.log
 @find "Warning" < build.log
 @find "Error" < build.log
