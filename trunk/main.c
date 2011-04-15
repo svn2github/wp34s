bd59e5be8bf563cb8cdd177eb491fe0dd9523312
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-04-15 10:02:08 +0000

    SVN keyword property changes.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@621 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/main.c
+++ trunk/main.c
@@ -18,7 +18,7 @@
  * This is the main module for the real hardware
  * Module written by MvC
  */
-__attribute__((section(".revision"),externally_visible)) const char SvnRevision[ 12 ] = "$Rev:: 1000$";
+__attribute__((section(".revision"),externally_visible)) const char SvnRevision[ 12 ] = "$Rev::     $";
 
 #include "xeq.h"
 #include "display.h"
