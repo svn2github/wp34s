99f934d2817478a9862586661636e5c90ecec136
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-05-05 21:45:22 +0000

    revert word size X change -- the 16c does something different here
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@822 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/xeq.c
+++ trunk/xeq.c
@@ -1964,14 +1964,7 @@ void cmdflag(unsigned int arg, enum rarg op) {
 }
 
 void intws(unsigned int arg, enum rarg op) {
-	int im = is_intmode();
-	long long int v;
-
-	if (im)
-		v = d64toInt(&regX);
 	State.int_len = arg;
-	if (im)
-		d64fromInt(&regX, mask_value(v));	
 }
 
 
