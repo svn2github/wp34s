4fc81ee80ed43885e81b35cefc902ba3cc441eb9
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-06-09 06:20:03 +0000

    integration build - register browser and extended catalogues
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1043 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/wp34sgui/builddate.h
+++ trunk/windows/wp34sgui/builddate.h
@@ -4,5 +4,5 @@ unsigned long long const BuildDate
 #pragma section = "version"
 @ "version"
 #endif
-= 0x7608201100000000;
-#define SVN_REVISION "$Rev:: 1234  $"
+= 0x7609201100000000;
+#define SVN_REVISION "$Rev:: 263   $"
