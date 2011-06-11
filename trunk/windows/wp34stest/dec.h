af98c689df95eae94d57f9f0099e1ecfbeb2ea4a
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-06-11 02:02:22 +0000

    fixing problems from context removal
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1072 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/wp34stest/dec.h
+++ trunk/windows/wp34stest/dec.h
@@ -21,36 +21,36 @@ public:
     {
         char buf[32];
         sprintf(buf, "%d", v);
-        decNumberFromString(&_v, buf, Ctx);
+        decNumberFromString(&_v, buf);
     }
 
     // operators
     friend Dec operator+(const Dec& a, const Dec& b)
-    { decNumber r; decNumberAdd(&r, &a._v, &b._v, Ctx); return r; }
+    { decNumber r; dn_add(&r, &a._v, &b._v); return r; }
 
     friend Dec operator-(const Dec& a, const Dec& b)
-    { decNumber r; decNumberSubtract(&r, &a._v, &b._v, Ctx); return r; }
+    { decNumber r; dn_subtract(&r, &a._v, &b._v); return r; }
 
     friend Dec operator*(const Dec& a, const Dec& b)
-    { decNumber r; decNumberMultiply(&r, &a._v, &b._v, Ctx); return r; }
+    { decNumber r; dn_multiply(&r, &a._v, &b._v); return r; }
 
     friend Dec operator/(const Dec& a, const Dec& b)
-    { decNumber r; decNumberDivide(&r, &a._v, &b._v, Ctx); return r; }
+    { decNumber r; dn_divide(&r, &a._v, &b._v); return r; }
 
     Dec& operator+=(const Dec& a)
-    { decNumberAdd(&_v, &_v, &a._v, Ctx);  return *this; }
+    { dn_add(&_v, &_v, &a._v);  return *this; }
 
     Dec& operator-=(const Dec& a)
-    { decNumberSubtract(&_v, &_v, &a._v, Ctx);  return *this; }
+    { dn_subtract(&_v, &_v, &a._v);  return *this; }
 
     Dec& operator*=(const Dec& a)
-    { decNumberMultiply(&_v, &_v, &a._v, Ctx);  return *this; }
+    { dn_multiply(&_v, &_v, &a._v);  return *this; }
 
     Dec& operator/=(const Dec& a)
-    { decNumberDivide(&_v, &_v, &a._v, Ctx);  return *this; }
+    { dn_divide(&_v, &_v, &a._v);  return *this; }
 
     Dec operator-() const
-    { decNumber r; decNumberMinus(&r, &_v, Ctx); return r; }
+    { decNumber r; dn_minus(&r, &_v); return r; }
 
     const char* asString() const
     {
@@ -61,22 +61,22 @@ public:
     }
 
     Dec sqrt() const
-    { decNumber r; decNumberSquareRoot(&r, &_v, Ctx); return r; }
+    { decNumber r; dn_sqrt(&r, &_v); return r; }
 
-    Dec ln() const { decNumber r; decNumberLn(&r, &_v, Ctx); return r; }
-    Dec sin() const { decNumber r; decNumberSin(&r, &_v, Ctx); return r; }
-    Dec cos() const { decNumber r; decNumberCos(&r, &_v, Ctx); return r; }
-    Dec tan() const { decNumber r; decNumberTan(&r, &_v, Ctx); return r; }
-    Dec exp() const { decNumber r; decNumberExp(&r, &_v, Ctx); return r; }
+    Dec ln() const { decNumber r; dn_ln(&r, &_v); return r; }
+    Dec sin() const { decNumber r; decNumberSin(&r, &_v); return r; }
+    Dec cos() const { decNumber r; decNumberCos(&r, &_v); return r; }
+    Dec tan() const { decNumber r; decNumberTan(&r, &_v); return r; }
+    Dec exp() const { decNumber r; decNumberExp(&r, &_v); return r; }
 
-    Dec asin() const { decNumber r; decNumberArcSin(&r, &_v, Ctx); return r; }
-    Dec acos() const { decNumber r; decNumberArcCos(&r, &_v, Ctx); return r; }
-    Dec atan() const { decNumber r; decNumberArcTan(&r, &_v, Ctx); return r; }
+    Dec asin() const { decNumber r; decNumberArcSin(&r, &_v); return r; }
+    Dec acos() const { decNumber r; decNumberArcCos(&r, &_v); return r; }
+    Dec atan() const { decNumber r; decNumberArcTan(&r, &_v); return r; }
 
-    Dec sinh() const { decNumber r; decNumberSinh(&r, &_v, Ctx); return r; }
-    Dec cosh() const { decNumber r; decNumberCosh(&r, &_v, Ctx); return r; }
-    Dec tanh() const { decNumber r; decNumberTanh(&r, &_v, Ctx); return r; }
-    Dec factorial() const { decNumber r; decNumberFactorial(&r, &_v, Ctx); return r; }
+    Dec sinh() const { decNumber r; decNumberSinh(&r, &_v); return r; }
+    Dec cosh() const { decNumber r; decNumberCosh(&r, &_v); return r; }
+    Dec tanh() const { decNumber r; decNumberTanh(&r, &_v); return r; }
+    Dec factorial() const { decNumber r; decNumberFactorial(&r, &_v); return r; }
 
 
 private:
