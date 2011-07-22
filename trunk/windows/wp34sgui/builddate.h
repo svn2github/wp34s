92b51bc69bdead3e1ca3eb24ef6c7568cdcb0ad6
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-07-22 22:42:56 +0000

    integration build with new constants
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1283 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/wp34sgui/builddate.h
+++ trunk/windows/wp34sgui/builddate.h
@@ -4,5 +4,5 @@ unsigned long long const BuildDate
 #pragma section = "version"
 @ "version"
 #endif
-= 0x7722201100000000;
-#define SVN_REVISION "$Rev:: 1234  $"
+= 0x7723201100000000;
+#define SVN_REVISION "$Rev:: 263   $"
