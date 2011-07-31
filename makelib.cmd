bbfbfd7ad3416533497af226c034753165fc54e5
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-07-31 16:23:49 +0000

    Added library generation stuff
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1372 9654ca62-1852-461c-aa49-1be32f3bb02d

---
+++ makelib.cmd
@@ -0,0 +1,4 @@
+setlocal
+cd library
+set TOOLS=..\trunk\tools
+%TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op TVM.wp34s WHO.wp34s 8queens.wp34s code_breaker.wp34s primesieve.wp34s quaternions.wp34s vectors.wp34s -o wp34s-1.dat
