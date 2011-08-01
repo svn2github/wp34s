1e2127693705f8073ef8ab3439101b3cbcac9ef8
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-08-01 09:26:40 +0000

    Updated library (vectors)
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1375 9654ca62-1852-461c-aa49-1be32f3bb02d

--- makelib.cmd
+++ makelib.cmd
@@ -1,5 +1,8 @@
+@echo off
 setlocal
 cd library
 set TOOLS=..\trunk\tools
 %TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op TVM.wp34s WHO.wp34s 8queens.wp34s code_breaker.wp34s primesieve.wp34s quaternions.wp34s vectors.wp34s -o wp34s-1.dat
+if errorlevel 1 goto exit
 copy wp34s-1.dat ..\trunk\windows\wp34sgui
+:exit
