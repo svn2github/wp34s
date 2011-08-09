a5c94084747b83ea1ba0364ec4e45f3a32aa3e27
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-08-09 11:40:51 +0000

    Integration build & Library update.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1416 9654ca62-1852-461c-aa49-1be32f3bb02d

--- makelib.cmd
+++ makelib.cmd
@@ -2,7 +2,11 @@
 setlocal
 cd library
 set TOOLS=..\trunk\tools
-%TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op TVM.wp34s WHO.wp34s 8queens.wp34s code_breaker.wp34s primesieve.wp34s quaternions.wp34s vectors.wp34s -o wp34s-1.dat
+rem %TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op TVM.wp34s WHO.wp34s 8queens.wp34s code_breaker.wp34s primesieve.wp34s quaternions.wp34s vectors.wp34s -o wp34s-1.dat
+%TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op TVM.wp34s vectors.wp34s WHO.wp34s -o wp34s-1.dat
 if errorlevel 1 goto exit
 copy wp34s-1.dat ..\trunk\windows\wp34sgui
+%TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op 8queens.wp34s code_breaker.wp34s primesieve.wp34s quaternions.wp34s -o wp34s-2.dat
+if errorlevel 1 goto exit
+copy wp34s-2.dat ..\trunk\windows\wp34sgui
 :exit
