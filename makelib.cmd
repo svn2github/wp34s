ab78d9885fdfd69cb83bae77f90fcd2727d62db6
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-08-10 13:09:59 +0000

    Library updated to new alpha command
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1427 9654ca62-1852-461c-aa49-1be32f3bb02d

--- makelib.cmd
+++ makelib.cmd
@@ -6,7 +6,7 @@ rem %TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op TVM.wp34s WHO.wp34s 8queens.wp34s
 %TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op TVM.wp34s vectors.wp34s -o wp34s-1.dat
 if errorlevel 1 goto exit
 copy wp34s-1.dat ..\trunk\windows\wp34sgui
-%TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op 8queens.wp34s code_breaker.wp34s primesieve.wp34s quaternions.wp34s -o wp34s-2.dat
+%TOOLS%\wp34s_asm.exe -op %TOOLS%\wp34s.op WHO.wp34s 8queens.wp34s code_breaker.wp34s primesieve.wp34s quaternions.wp34s -o wp34s-2.dat
 if errorlevel 1 goto exit
 copy wp34s-2.dat ..\trunk\windows\wp34sgui
 :exit
