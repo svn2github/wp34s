3efd16860ecff02df52bd7ce23a5c5eb972a3665
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-08-20 23:26:48 +0000

    integration build with 4+12 show function
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1480 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/wp34sgui/builddate.h
+++ trunk/windows/wp34sgui/builddate.h
@@ -4,5 +4,5 @@ unsigned long long const BuildDate
 #pragma section = "version"
 @ "version"
 #endif
-= 0x7819201100000000;
-#define SVN_REVISION "$Rev:: 1234  $"
+= 0x7821201100000000;
+#define SVN_REVISION "$Rev:: 263   $"
