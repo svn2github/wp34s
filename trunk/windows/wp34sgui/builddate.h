382c0a2392a4f24d85c62c79c77101af1487f76b
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-09-07 10:21:02 +0000

    integration build with changed multi-alpha opcodes
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1564 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/wp34sgui/builddate.h
+++ trunk/windows/wp34sgui/builddate.h
@@ -4,5 +4,5 @@ unsigned long long const BuildDate
 #pragma section = "version"
 @ "version"
 #endif
-= 0x7901201100000000;
-#define SVN_REVISION "$Rev:: 1234  $"
+= 0x7907201100000000;
+#define SVN_REVISION "$Rev:: 263   $"
