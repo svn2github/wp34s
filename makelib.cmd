cf1eac8f4caa117c07035d4d61384c2ab64c43f4
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-10-05 14:51:18 +0000

    Library updates, libs 1 to 5.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1685 9654ca62-1852-461c-aa49-1be32f3bb02d

--- makelib.cmd
+++ makelib.cmd
@@ -3,23 +3,34 @@ setlocal
 cd library
 set TOOLS=..\trunk\tools
 set ASM=%TOOLS%\wp34s_asm.pl -op %TOOLS%\wp34s.op -fill 0xffff
+set PP=%TOOLS%\wp34s_pp.pl
 
-%ASM% TVM.wp34s vectors.wp34s -o wp34s-1.dat
+set DAT=wp34s-1.dat
+%ASM% -pp matrixedit.wp34s vectors_pp.wp34s -o %DAT%
 if errorlevel 1 goto exit
-copy wp34s-1.dat ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\windows\wp34sgui
 
+set DAT=wp34s-2.dat
+%ASM% TVM.wp34s -o %DAT%
+if errorlevel 1 goto exit
+copy %DAT% ..\trunk\windows\wp34sgui
+
+set DAT=wp34s-3.dat
+%ASM% -pp TRIGON.wp34s PF.wp34s -o %DAT%
+if errorlevel 1 goto exit
+copy %DAT% ..\trunk\windows\wp34sgui
+
+set DAT=wp34s-4.dat
 set SRC=8queens.wp34s code_breaker_pp.wp34s 
 set SRC=%SRC% primesieve_pp.wp34s quaternions.wp34s
 set SRC=%SRC% HHC2010_Challenge.wp34s savage.wp34s
-set SRC=%SRC% PF.wp34s
-%ASM% -pp %SRC% -o wp34s-2.dat
+%ASM% -pp %SRC% -o %DAT%
 if errorlevel 1 goto exit
-copy wp34s-2.dat ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\windows\wp34sgui
 
-set SRC=TRIGON.wp34s 
-rem set SRC=%SRC% matrix.wp34s 
-%ASM% -pp %SRC% -o wp34s-3.dat
+set DAT=wp34s-5.dat
+%ASM% -pp matrixtest.wp34s -o %DAT%
 if errorlevel 1 goto exit
-copy wp34s-3.dat ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\windows\wp34sgui
 
 :exit
