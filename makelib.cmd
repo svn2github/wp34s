2e8fd9cedccef945b9adf7d66e5e659e82c07a5a
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-10-14 07:43:06 +0000

    Library update
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1726 9654ca62-1852-461c-aa49-1be32f3bb02d

--- makelib.cmd
+++ makelib.cmd
@@ -6,19 +6,22 @@ set ASM=%TOOLS%\wp34s_asm.pl -op %TOOLS%\wp34s.op -fill 0xffff
 set PP=%TOOLS%\wp34s_pp.pl
 
 set DAT=wp34s-1.dat
-%ASM% -pp matrixedit.wp34s vectors_pp.wp34s -o %DAT%
+%ASM% -pp matrix.wp34s matrixedit.wp34s vectors_pp.wp34s -o %DAT%
 if errorlevel 1 goto exit
 copy %DAT% ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\realbuild
 
 set DAT=wp34s-2.dat
 %ASM% TVM.wp34s -o %DAT%
 if errorlevel 1 goto exit
 copy %DAT% ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\realbuild
 
 set DAT=wp34s-3.dat
 %ASM% -pp TRIGON.wp34s PF.wp34s -o %DAT%
 if errorlevel 1 goto exit
 copy %DAT% ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\realbuild
 
 set DAT=wp34s-4.dat
 set SRC=8queens.wp34s code_breaker_pp.wp34s 
@@ -27,10 +30,12 @@ set SRC=%SRC% HHC2010_Challenge.wp34s savage.wp34s
 %ASM% -pp %SRC% -o %DAT%
 if errorlevel 1 goto exit
 copy %DAT% ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\realbuild
 
 set DAT=wp34s-5.dat
 %ASM% -pp matrixtest.wp34s -o %DAT%
 if errorlevel 1 goto exit
 copy %DAT% ..\trunk\windows\wp34sgui
+copy %DAT% ..\trunk\realbuild
 
 :exit
