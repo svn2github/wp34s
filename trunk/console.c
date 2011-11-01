c70c46308f19a3030297ebbf18516354cec4d4b7
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-11-01 22:31:17 +0000

    Output number of program steps in a region at the start of the opcode file
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@1807 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/console.c
+++ trunk/console.c
@@ -415,6 +415,7 @@ int main(int argc, char *argv[]) {
 				goto skipargs;
 			}
 			if (strcmp(argv[1], "opcodes") == 0) {
+				printf("maxsteps=%d\n", NUMPROG);
 				dump_opcodes(stdout);
 				return 0;
 			}
