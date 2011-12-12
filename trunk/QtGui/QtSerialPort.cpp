ddb7d9dcfaf2fd7d4a867ada0ce06d1179ae70ab
pmeheut <pmeheut@9654ca62-1852-461c-aa49-1be32f3bb02d>
2011-12-12 07:54:00 +0000

    Fix a bug in QtSerialPort.cpp preventing compilation on Windows
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2023 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/QtGui/QtSerialPort.cpp
+++ trunk/QtGui/QtSerialPort.cpp
@@ -17,9 +17,10 @@
 #include "QtSerialPort.h"
 #include <qextserialenumerator.h>
 #include <QList>
-
+#include "QtEmulator.h"
 
 QtSerialPort::QtSerialPort()
+: serialPort(NULL)
 {
 }
 
@@ -31,6 +32,36 @@ const QString& QtSerialPort::getSerialPortName() const
 void QtSerialPort::setSerialPortName(const QString& aSerialPortName)
 {
 	serialPortName=aSerialPortName;
+}
+
+bool QtSerialPort::open(const PortSettings& thePortSettings)
+{
+	close();
+	serialPort=new QextSerialPort(thePortSettings, QextSerialPort::EventDriven);
+	serialPort->setBaudRate(thePortSettings.BaudRate);
+	serialPort->setDataBits(thePortSettings.DataBits);
+	serialPort->setParity(thePortSettings.Parity);
+	serialPort->setStopBits(thePortSettings.StopBits);
+	serialPort->setFlowControl(FLOW_OFF);
+	bool opened=serialPort->open(QIODevice::ReadWrite);
+	if(opened)
+	{
+// Add connect here
+	}
+	return opened;
+}
+
+void QtSerialPort::close()
+{
+	if(serialPort!=NULL)
+	{
+		if(serialPort->isOpen())
+		{
+			serialPort->close();
+		}
+		delete serialPort;
+		serialPort=NULL;
+	}
 }
 
 QStringList QtSerialPort::getSerialPorts()
@@ -49,15 +80,72 @@ extern "C"
 {
 int open_port(int baud, int bits, int parity, int stopbits)
 {
-	Q_UNUSED(baud)
-	Q_UNUSED(bits)
-	Q_UNUSED(parity)
-	Q_UNUSED(stopbits)
+	PortSettings portSettings;
+	switch(baud)
+	{
+#ifndef Q_WS_WIN
+		case 50: { portSettings.BaudRate=BAUD50; break; }
+		case 75: { portSettings.BaudRate=BAUD75; break; }
+#endif
+		case 110: { portSettings.BaudRate=BAUD110; break; }
+#ifndef Q_WS_WIN
+		case 134: { portSettings.BaudRate=BAUD134; break; }
+		case 150: { portSettings.BaudRate=BAUD150; break; }
+		case 200: { portSettings.BaudRate=BAUD200; break; }
+#endif
+		case 300: { portSettings.BaudRate=BAUD300; break; }
+		case 600: { portSettings.BaudRate=BAUD600; break; }
+		case 1200: { portSettings.BaudRate=BAUD1200; break; }
+#ifndef Q_WS_WIN
+		case 1800: { portSettings.BaudRate=BAUD1800; break; }
+#endif
+		case 2400: { portSettings.BaudRate=BAUD2400; break; }
+		case 4800: { portSettings.BaudRate=BAUD4800; break; }
+		case 9600: { portSettings.BaudRate=BAUD9600; break; }
+		case 14400: { portSettings.BaudRate=BAUD14400; break; }
+		case 19200: { portSettings.BaudRate=BAUD19200; break; }
+		case 38400: { portSettings.BaudRate=BAUD38400; break; }
+		case 56000: { portSettings.BaudRate=BAUD56000; break; }
+		case 57600: { portSettings.BaudRate=BAUD57600; break; }
+#ifndef Q_WS_WIN
+		case 76800: { portSettings.BaudRate=BAUD76800; break; }
+#endif
+		case 115200: { portSettings.BaudRate=BAUD115200; break; }
+		case 128000: { portSettings.BaudRate=BAUD128000; break; }
+		case 256000: { portSettings.BaudRate=BAUD256000; break; }
+		default: { portSettings.BaudRate=BAUD9600; break; }
+	}
 
-	return 0;
+	switch(bits)
+	{
+		case 5: { portSettings.DataBits=DATA_5; break; }
+		case 6: { portSettings.DataBits=DATA_6; break; }
+		case 7: { portSettings.DataBits=DATA_7; break; }
+		case 8: { portSettings.DataBits=DATA_8; break; }
+		default: { portSettings.DataBits=DATA_8; break; }
+	}
+
+	switch(parity)
+	{
+		case 'E': { portSettings.Parity=PAR_EVEN; break; }
+		case 'O': { portSettings.Parity=PAR_ODD; break; }
+		case 'M': { portSettings.Parity=PAR_MARK; break; }
+		case 'S': { portSettings.Parity=PAR_SPACE; break; }
+		case 'N': { portSettings.Parity=PAR_NONE; break; }
+		default: { portSettings.Parity=PAR_NONE; break; }
+	}
+
+	switch(stopbits)
+	{
+		case 1: { portSettings.StopBits=STOP_1; break; }
+		case 2: { portSettings.StopBits=STOP_2; break; }
+		default: { portSettings.StopBits=STOP_1; break; }
+	}
+
+	return currentEmulator->getSerialPort().open(portSettings);
 }
 
-extern void close_port()
+void close_port()
 {
 }
 
