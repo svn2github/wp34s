682c3d3717835013388ffe8911b17f315bd4cdfd
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-01-01 23:46:08 +0000

    include sizes for Lambert's W
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2130 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/unused/lambertW.c
+++ trunk/unused/lambertW.c
@@ -1,4 +1,4 @@
-// 32 bytes
+// real inverse LamW - 32 bytes
 decNumber *decNumberInvW(decNumber *r, const decNumber *x) {
 	decNumber t;
 
@@ -7,10 +7,18 @@ decNumber *decNumberInvW(decNumber *r, const decNumber *x) {
 }
 
 
-// 44 bytes
+// real LamW - 432 bytes
+
+
+
+
+
+// complex inverse LamW - 44 bytes
 void cmplxInvW(decNumber *rx, decNumber *ry, const decNumber *a, const decNumber *b) {
 	decNumber t1, t2;
 
 	cmplxExp(&t1, &t2, a, b);
 	cmplxMultiply(rx, ry, &t1, &t2, a, b);
 }
+
+// complex LamW - 624 bytes
