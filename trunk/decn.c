f5abc99ca4f76fabe3da6ef1f779774682fc456a
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-01-13 04:16:32 +0000

    Unroll the normal distribution changes in anticipation of v3 release.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2237 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/decn.c
+++ trunk/decn.c
@@ -2244,6 +2244,17 @@ decNumber *decNumberERF(decNumber *res, const decNumber *x) {
 	
 }
 
+// Complementary error function
+decNumber *decNumberERFC(decNumber *res, const decNumber *x) {
+	decNumber a, b;
+
+	dn_divide(&a, x, &const_root2on2);
+	dn_minus(&b, &a);
+	cdf_Q(&a, &b);
+	return dn_mul2(res, &a);
+}
+
+
 
 /* Jacobi Elliptical functions */
 #ifdef INCLUDE_ELLIPTIC
