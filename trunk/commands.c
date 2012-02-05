604eb8c9e46f806b0c3505b9f93d042ec9fa2906
paulidale <paulidale@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-02-05 23:25:23 +0000

    rename RND to RSD
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2374 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/commands.c
+++ trunk/commands.c
@@ -849,7 +849,7 @@ const struct argcmd argcmds[ NUM_RARG ] = {
 
 	CMDstk(RARG_VIEW_REG,	&alpha_view_reg,			"VW\240+")
 	CMD(RARG_ROUNDING,	&rarg_roundingmode, DEC_ROUND_MAX,	"RM")
-	CMD(RARG_ROUND,		&rarg_round, 34,			"RND")
+	CMD(RARG_ROUND,		&rarg_round, 34,			"RSD")
 
 #ifdef INCLUDE_USER_MODE
 	CMDstk(RARG_STOM,	&cmdsavem,				"STOM")
