4b8247102dc3a1b4f93014f150cd777e4ad89636
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-02-11 06:59:51 +0000

    Renamed CNST to # and INT# to #INT to retain sort order.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2434 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/commands.c
+++ trunk/commands.c
@@ -880,10 +880,10 @@ const struct argcmd argcmds[ NUM_RARG ] = {
 	CMDnoI(RARG_CONVERGED,	&cmdconverged,	32,			"CNVG?")
 	CMDnoI(RARG_SHUFFLE,	&cmdshuffle,	256,			"\027")
 
-	CMDnoI(RARG_INTNUM,	  &cmdintnum,	256,			"INT#")
-	CMDnoI(RARG_INTNUM_CMPLX, &cmdintnum,	256,			"\024INT#")
-	CMD(RARG_IND_CONST,	  &cmdconst,	NUM_CONSTS,		"CNST")
-	CMD(RARG_IND_CONST_CMPLX, &cmdconst,	NUM_CONSTS,		"\024CNST")
+	CMDnoI(RARG_INTNUM,	  &cmdintnum,	256,			"#INT")
+	CMDnoI(RARG_INTNUM_CMPLX, &cmdintnum,	256,			"\024#INT")
+	CMD(RARG_IND_CONST,	  &cmdconst,	NUM_CONSTS,		"#")
+	CMD(RARG_IND_CONST_CMPLX, &cmdconst,	NUM_CONSTS,		"\024#")
 
 #undef CMDlbl
 #undef CMDlblnI
