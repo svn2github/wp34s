c4546629ee2bb26a57f7022049ef52fc9967df3f
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-02-21 18:01:59 +0000

    Cleanup
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2532 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/winfont/make_fonts.cmd
+++ trunk/windows/winfont/make_fonts.cmd
@@ -3,24 +3,5 @@ setlocal
 set bindir=Release
 if not "%1"=="" set bindir=%1
 
-goto 48
-%bindir%\winfont wp34s_regular_6.fnt 1 0
-%bindir%\winfont wp34s_regular_12.fnt 2 0
-%bindir%\winfont wp34s_regular_18.fnt 3 0
-%bindir%\winfont wp34s_regular_24.fnt 4 1
-%bindir%\winfont wp34s_regular_30.fnt 5 1
-%bindir%\winfont wp34s_regular_36.fnt 6 1
-%bindir%\winfont wp34s_regular_42.fnt 7 2
-:48
-%bindir%\winfont wp34s_regular_48.fnt 8 2
-
-goto 40
-%bindir%\winfont -s wp34s_small_5.fnt 1 0
-%bindir%\winfont -s wp34s_small_10.fnt 2 0
-%bindir%\winfont -s wp34s_small_15.fnt 3 0
-%bindir%\winfont -s wp34s_small_20.fnt 4 1
-%bindir%\winfont -s wp34s_small_25.fnt 5 1
-%bindir%\winfont -s wp34s_small_30.fnt 6 1
-%bindir%\winfont -s wp34s_small_35.fnt 7 2
-:40
-%bindir%\winfont -s wp34s_small_40.fnt 8 2
+%bindir%\winfont WP34SRasterFontRegular.sfd  Template.sfd 20
+%bindir%\winfont -s WP34SRAsterFontSmall.sfd Template.sfd 20
