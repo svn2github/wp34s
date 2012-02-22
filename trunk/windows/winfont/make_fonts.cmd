d31ea2defce06cd8438e34ef75e0f4ac4b2f1878
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-02-22 15:12:42 +0000

    Added solid fonts whithout gaps between pixels.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2543 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/winfont/make_fonts.cmd
+++ trunk/windows/winfont/make_fonts.cmd
@@ -4,4 +4,7 @@ set bindir=Release
 if not "%1"=="" set bindir=%1
 
 %bindir%\winfont -u WP34SRasterFontRegular.sfd  Template.sfd 20
-%bindir%\winfont -u -s WP34SRAsterFontSmall.sfd Template.sfd 20
+%bindir%\winfont -u -s WP34SRasterFontSmall.sfd Template.sfd 20
+
+%bindir%\winfont -u WP34SSolidFontRegular.sfd  TemplateSolid.sfd 0
+%bindir%\winfont -u -s WP34SSolidFontSmall.sfd TemplateSolid.sfd 0
