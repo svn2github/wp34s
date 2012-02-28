21c0a14bc5d1a8e07eea70670ef4aef2db2d9d3c
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-02-28 08:27:11 +0000

    Yet another attempt at the prefix chars. Built for testing.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2597 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/windows/winfont/make_fonts.cmd
+++ trunk/windows/winfont/make_fonts.cmd
@@ -10,3 +10,15 @@ if not "%1"=="" set bindir=%1
 %bindir%\winfont -u -s WP34SSolidFontSmall.sfd TemplateSolid.sfd 0
 
 %bindir%\winfont -7 WP34SSegmentFont.sfd TemplateSegment.sfd
+
+set FF=C:\APPS\MinGW\fontforge\fontforge.bat
+if not exist %FF% goto exit
+set FF=%FF% -c "Open($1);Generate($2);"
+call %FF% WP34SRasterFontRegular.sfd WP34SRasterFontRegular.otf
+call %FF% WP34SRasterFontSmall.sfd   WP34SRasterFontSmall.otf
+call %FF% WP34SSolidFontRegular.sfd  WP34SSolidFontRegular.otf
+call %FF% WP34SSolidFontSmall.sfd    WP34SSolidFontSmall.otf
+call %FF% WP34SSegmentFont.sfd       WP34SSegmentFont.otf
+
+:exit
+endlocal
