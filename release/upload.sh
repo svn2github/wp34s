127f7bfda8dee12f142276bf93d0466698763e35
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-03-02 12:34:09 +0000

    Integration build
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@2624 9654ca62-1852-461c-aa49-1be32f3bb02d

--- release/upload.sh
+++ release/upload.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 
-files="wp34s/revision.txt wp34s_V3.zip"
+files="wp34s/revision.txt wp34s_V3.zip wp34s_Fonts.zip"
 docs="wp34s/doc/*.pdf"
 user="mvcube,wp34s"
 dest="frs.sourceforge.net:/home/frs/project/w/wp/wp34s"
 
 echo README.TXT needs to be updated from the web interface
-#scp $docs $user@$dest/doc
+scp $docs $user@$dest/doc
 scp $files $user@$dest
