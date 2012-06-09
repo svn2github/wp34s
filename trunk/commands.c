e9f4bb6a7340c6cfbe8b9ca0fdd26334fae1d77e
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2012-06-09 08:04:40 +0000

    Renamed the plotting commands to gSET, etc.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@3121 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/commands.c
+++ trunk/commands.c
@@ -940,13 +940,13 @@ const struct argcmd argcmds[ NUM_RARG ] = {
 	CMD(RARG_PDELAY,	IRA(cmdprintmode),  32,			"\222DLAY",	"P.DLAY")
 	CMDcstk(RARG_PRINT_CMPLX, IRA(cmdprintcmplxreg),		"\222\024r\213\214",	"P.crect")
 #ifdef INCLUDE_PLOTTING
-	CMDplt(RARG_PLOT_INIT,    &cmdplotinit,				"\222INIT",	"P.INIT")
-	CMDplt(RARG_PLOT_WIDTH,   &cmdplotwidth,			"\222WID?",	"P.WID?")
-	CMDplt(RARG_PLOT_SETPIX,  &cmdplotpixel,			"\222SET",	"P.SET")
-	CMDplt(RARG_PLOT_CLRPIX,  &cmdplotpixel,			"\222CLR",	"P.CLR")
-	CMDplt(RARG_PLOT_FLIPPIX, &cmdplotpixel,			"\222FLP",	"P.FLP")
-	CMDplt(RARG_PLOT_ISSET,   &cmdplotpixel,			"\222PIX?",	"P.PIX?")
-	CMDplt(RARG_PLOT_DISPLAY, &cmdplotdisplay,			"PLOT",		CNULL)
+	CMDplt(RARG_PLOT_INIT,    &cmdplotinit,				"gINIT",	CNULL)
+	CMDplt(RARG_PLOT_WIDTH,   &cmdplotwidth,			"gWID?",	CNULL)
+	CMDplt(RARG_PLOT_SETPIX,  &cmdplotpixel,			"gSET",		CNULL)
+	CMDplt(RARG_PLOT_CLRPIX,  &cmdplotpixel,			"gCLR",		CNULL)
+	CMDplt(RARG_PLOT_FLIPPIX, &cmdplotpixel,			"gFLP",		CNULL)
+	CMDplt(RARG_PLOT_ISSET,   &cmdplotpixel,			"gPIX?",	CNULL)
+	CMDplt(RARG_PLOT_DISPLAY, &cmdplotdisplay,			"gPLOT",	CNULL)
 	CMDplt(RARG_PLOT_PRINT,   IRA(cmdplotprint),			"\222PLOT",	"P.PLOT")
 #endif
 	/* end of INFRARED commands */
