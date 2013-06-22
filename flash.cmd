e55a89649ca1483bdb98e0f51f1439f7dadf28b2
mvcube <mvcube@9654ca62-1852-461c-aa49-1be32f3bb02d>
2013-06-22 16:34:18 +0000

    Integration build with GCC 4.6.0
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@3418 9654ca62-1852-461c-aa49-1be32f3bb02d

--- flash.cmd
+++ flash.cmd
@@ -2,7 +2,7 @@
 setlocal
 rem PATH C:\APPS\netX-ARM-GCC\bin;%PATH%
 rem PATH C:\APPS\yagarto-4.5.2\bin;%PATH%
-rem PATH C:\APPS\yagarto-4.6.0\bin;%PATH%
+PATH C:\APPS\yagarto-4.6.0\bin;%PATH%
 rem PATH C:\APPS\yagarto-4.7.2\bin;%PATH%
 rem PATH C:\APPS\CodeSourcery\bin;%PATH%
 cd trunk
