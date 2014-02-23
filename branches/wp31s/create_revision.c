13bae966fef2bac808ff6d085f66e9810367aee7
jmcameron <jmcameron@9654ca62-1852-461c-aa49-1be32f3bb02d>
2014-02-23 04:49:43 +0000

    Refactored to allow getting the svn verson number from svn or git.
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@3513 9654ca62-1852-461c-aa49-1be32f3bb02d

--- branches/wp31s/create_revision.c
+++ branches/wp31s/create_revision.c
@@ -28,64 +28,125 @@ static char *Template =
 	"#endif\n"
 	"const char SvnRevision[ 4 ] = \"%-4d\";\n";
 
+int get_revision_num(char *vcs_cmd, char *vcs);
+
+
 int main( int argc, char **argv )
 {
-	char buffer[ 100 ];
-	char tmpname[ FILENAME_MAX ];
-	FILE *f;
-	int rev;
-	char *p;
+    int rev;
+
+    // Get the revision number from subversion
+    rev = get_revision_num("svnversion -n >%s", "subversion");
+    if (rev < 0)
+    {
+	rev = get_revision_num("git svn find-rev `git rev-parse master` >%s", "subversion");
+    }
+
+    // If neither svn nor git can give us an answer, just assume 0
+    if (rev < 0)
+    {
+	rev = 0;
+    }
+
+    // Increment
+    if ( rev != 0 ) {
+	// Assume the next higher revision number on next commit
+	++rev;
+	}
+    fprintf( stderr, "    Revision number will be %d\n\n", rev );
+
+    // Create output
+    license(stdout, "/* ", " * ", " */");
+    printf( Template, rev );
+
+    return 0;
+}
+
+
+
+
+// Get VCS revision number 
+// (If there is an error, return a negative number)
+
+int get_revision_num(char *vcs_cmd, char *vcs)
+{
+    char buffer[ 100 ];
+    char tmpname[ FILENAME_MAX ];
+    char errmsg[256];
+    FILE *f;
+    char *p;
+    int i;
+    int result;
 	
-	if (tmpnam( tmpname ) == NULL) {
-		perror("Unable to create tempory file name");
-		return 1;
+    if (tmpnam( tmpname ) == NULL)
+    {
+	perror("Unable to create tempory file name");
+	return -1;
+    }
+
+    // Try to execute VCS command
+    sprintf( buffer, vcs_cmd, tmpname );
+    fprintf( stderr, "    Executing %s\n", buffer );
+    result = system(buffer);
+    if (result == -1)
+    {
+	// The VCS call failed
+	sprintf(errmsg, "Unable to run %s command", vcs);
+	perror(errmsg);
+	remove( tmpname );
+	return -1;
+    }
+
+    // Read result
+    f = fopen( tmpname, "r" );
+    if ( f == NULL )
+    {
+	sprintf(errmsg, "Opening the %s output file failed!", vcs);
+	perror(errmsg);
+	return -1;
+    }
+    else
+    {
+	if (fgets( buffer, sizeof( buffer ) - 1, f ) == NULL) 
+	{
+	    sprintf(errmsg, "Unable to read %s revision from temporary output file", vcs);
+	    perror(errmsg);
+	    fclose(f);
+	    remove(tmpname);
+	    return -1;
+	}
+	fclose(f);
+	remove(tmpname );
+
+	// Make sure we received something!
+	if ((strlen(buffer) == 0) ||
+	    (strncmp(buffer, "exported", 100) == 0))
+	{
+	    fprintf(stderr, "    No revision number obtained from %s \n", vcs);
+	    return -1;
 	}
 
-	// Try to execute svnversion
-	sprintf( buffer, "svnversion -n >%s", tmpname );
-	fprintf( stderr, "Executing %s\n", buffer );
-	if (system( buffer ) == -1) {
-		perror("unable to run subversion command");
-		remove( tmpname );
-		return 1;
+	// Convert any LFs to spaces
+	for (i=0; i<strlen(buffer); i++)
+	{
+	    if (buffer[i] == '\n')
+		buffer[i] = ' ';
 	}
 
-	// Read result
-	f = fopen( tmpname, "r" );
-	if ( f == NULL ) {
-		fprintf( stderr, "Revision number is unknown\n" );
-		strcpy( buffer, "0000" );
-	}
-	else {
-		if (fgets( buffer, sizeof( buffer ) - 1, f ) == NULL) {
-			perror("unable to read revision");
-			fclose( f );
-			remove( tmpname );
-			return 1;
-		}
-		fprintf( stderr, "Revision number(s): %s\n", buffer );
-		fclose( f );
-		remove( tmpname );
-	}
+	fprintf(stderr, "    %s Revision number(s): %s\n", vcs, buffer);
+    }
 
-	// Determine the revision number
-	p = strchr( buffer, ':' );
-	if ( p == NULL ) {
-		p = buffer;
-	}
-	else {
-		++p;
-	}
-	rev = atoi( p );
-	if ( rev != 0 ) {
-		// Assume the next higher revision number on next commit
-		++rev;
-	}
-	fprintf( stderr, "Revision number will be %d\n", rev );
+    // Determine the revision number
+    p = strchr( buffer, ':' );
+    if ( p == NULL ) 
+    {
+	p = buffer;
+    }
+    else 
+    {
+	++p;
+    }
 
-	// Create output
-	license(stdout, "/* ", " * ", " */");
-	printf( Template, rev );
-
-	return 0;
+    // Parse the number
+    return atoi( p );
 }
