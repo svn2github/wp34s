fe5cb51a5845db8950375ca63bffec30393e9538
pmeheut <pmeheut@9654ca62-1852-461c-aa49-1be32f3bb02d>
2015-11-07 10:05:30 +0000

    Improved conditionals so that strtoull works
    git-svn-id: http://svn.code.sf.net/p/wp34s/code@3823 9654ca62-1852-461c-aa49-1be32f3bb02d

--- trunk/xeq.c
+++ trunk/xeq.c
@@ -24,7 +24,7 @@
 #include <sys/time.h>
 #endif
 #include <stdio.h>   // (s)printf
-#if !defined(WIN32)
+#if defined(QTGUI)
 extern unsigned long long strtoull(const char *, char **, int);
 #endif
 #endif // REALBUILD
